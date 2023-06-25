40d2d21c51bc0ea7b631fc8734c040c60e2eba7f
Andreza Ibanez <127691853+Andreza-Ibanez@users.noreply.github.com>
2023-06-24 22:45:34 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,19 @@
+alert ("Página carregada com sucesso!");
+
+var preçoArroz=20;
+
+var preçoFeijão=10;
+
+document.write ("Um pacote de arroz mais um de feijão dara:  " + (preçoArroz+preçoFeijão));
+
+document.write ("<br>");
+
+document.write ("Um pacote de arroz pequeno que é a metade do grande custara: " + (preçoArroz/2));
+
+document.write ("<br>");
+
+document.write ("Tres pacotes de feijão daram: " + (preçoFeijão*3));
+
+document.write ("<br>")
+
+document.write ("Arroz e Feijão são o mesmo preço: " + (preçoArroz==preçoFeijão));
